5b43c70185df0e23520c6e65bf92efe2992b8ec6
Jesus <jesusmueguz177@gmail.com>
2026-08-29 13:59:37 -0600

    commit de acontroladeres de usurio y de roles

---
+++ inventario/Controllers/UsuarioCoontroller.cs
@@ -0,0 +1,23 @@
+﻿using inventario.Models;
+using Microsoft.EntityFrameworkCore;
+using inventario.Data;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace inventario.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsuarioCoontroller:ControllerBase
+{
+    private readonly AppDbInventario _context;
+
+    public UsuarioCoontroller(AppDbInventario context)
+    {
+        _context = context; 
+    }
+    
+    
+
+}
