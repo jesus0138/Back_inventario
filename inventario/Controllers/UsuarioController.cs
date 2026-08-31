980919b98e2c6e3487c2b1fc6834356c07f0c78f
Jesus <jesusmueguz177@gmail.com>
2026-08-31 11:02:56 -0600

    commit de autenticacion de contrsena con hash

---
+++ inventario/Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿using inventario.Models;
+using Microsoft.EntityFrameworkCore;
+using inventario.Data;
+using BCrypt.Net;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace inventario.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsuarioController:ControllerBase
+{
+    private readonly AppDbInventario _context;
+
+    public UsuarioController(AppDbInventario context)
+    {
+        _context = context; 
+    }
+
+    [HttpGet]
+
+    public async Task<IActionResult> GetUsuarios()
+    {
+        var usuario = await _context.Usuarios.ToListAsync();
+        return Ok(usuario);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostUsuario(Usuario usuario)
+    {
+        bool existe=await _context.Usuarios
+            .AnyAsync(n=>n.NombreUsuario==usuario.NombreUsuario);
+        if (existe==true)
+        {
+            return BadRequest("El nombre de usuario ya existe");
+        }
+        usuario.Password = BCrypt.Net.BCrypt.HashPassword(usuario.Password);
+        _context.Usuarios.Add(usuario);
+        await _context.SaveChangesAsync();
+        return Ok();
+        
+    }
+    //necesito terminar de hacer el metodo delete y put
+}
---
+++ inventario/Dtos/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace inventario.Dtos;
+
+public class LoginDto
+{
+    
+        public required string NombreUsuario { get; set; }
+        public required string Password { get; set; }
+    
+}
